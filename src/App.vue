da8871875ee8797664fb00b235b335f7ed5afcd9
Marc Young <marcanthonyyoung@outlook.com>
2024-02-02 09:44:22 +0000

    e

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Rain/>
+  <!-- <Sun/> -->
+  <button @click="toggleRain">Toggle Rain</button>
+</template>
+
+<script>
+import Rain from './components/Rain.vue'
+import Sun from './components/Sun.vue'
+export default {
+  name: 'App',
+  components: {
+    Rain,Sun
+  },
+  data() {
+    return {
+      toggleRainBool: false
+    };
+  },
+  methods: {
+    toggleRain() {
+      this.toggleRainBool = !this.toggleRainBool;
+    }
+  }
+}
+</script>
+
